b52f44d48a42fc765bedd91ef642db26a2a0df68
Ddedalus <h.beres@hotmail.com>
2018-06-23 00:11:38 +0100

    Naive version of the ASP

---
+++ src/Calling C/bfs.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+// Number of nodes and edges
+uint32_t numNodes;
+uint32_t numEdges;
+//  An adjacency list representation
+vector< vector<uint32_t> > neighbours;
+
+void readGraph(string filename)
+{
+	ifstream fp;
+	fp.open(filename, ios::in);
+
+	// Read edge list from file
+	if (!fp) {
+		cout << "Can't open " << filename;
+		return;
+	}
+
+	// Create mapping from node id to number of neighbours
+	fp>>numNodes>>numEdges;
+	neighbours.resize(numNodes);
+	
+	uint32_t from, to;
+	for(uint32_t i = 0; i < numEdges; i++){
+		fp >> from >> to;
+		neighbours[from].push_back(to);
+	}
+	fp.close();
+	return;
+}
+
+uint32_t vertexSSP(uint32_t v){
+	uint32_t sum = 0;
+	vector<bool> visited(numNodes, false);
+	vector<uint32_t> fringe(numNodes);
+	vector<uint32_t> newFringe(numNodes);
+	fringe[0] = v;
+	uint32_t d = 1;	
+	while(! fringe.empty()){
+		for(uint32_t x : fringe){
+			for(auto n : neighbours[x]){
+				if(! visited[n]){
+					newFringe.push_back(n);
+					visited[n] = true;
+					sum+=d;
+				}
+			}
+		}
+		fringe.clear();
+		fringe.swap(newFringe);
+		d++;
+	}	
+	return sum;
+}
+
+double serialASP(){
+	double avg = 0.;
+	for(uint32_t v = 0; v < numNodes; v++){
+		avg += vertexSSP(v) / (1.0 * numNodes);
+	}
+	return avg /(1.0 * numNodes);
+
+}
+
+int main(){
+	string path = "/home/hubert/Code/Warwick/BSP/external/asp/Networks/n5.edges";
+	readGraph(path);
+	for(uint32_t v = 0; v < numNodes; v++){
+		// cout<<vertexSSP(v)<<endl;
+	}
+	cout<< serialASP()<<endl;
+	return 0;
+}
+
