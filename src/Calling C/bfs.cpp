7fa9508e4c39306e763212e9bfa138c4fce8e0a9
Ddedalus <h.beres@hotmail.com>
2018-06-23 13:26:46 +0100

    Queue BFS implementation

--- src/Calling C/bfs.cpp
+++ src/Calling C/bfs.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <queue>
 
 using namespace std;
 // Number of nodes and edges
@@ -20,94 +21,56 @@ void readGraph(string filename)
 		return;
 	}
 
-	// Create mapping from node id to number of neighbours
-	fp>>numNodes>>numEdges;
+	fp >> numNodes >> numEdges;
 	neighbours.resize(numNodes);
 	
 	uint32_t from, to;
 	for(uint32_t i = 0; i < numEdges; i++){
 		fp >> from >> to;
-		// version suitable for Higher computing
-		neighbours[min(from, to)].push_back(max(from, to));
+		neighbours[from].push_back(to);
+		neighbours[to].push_back(from);
 	}
 	fp.close();
 	return;
 }
 
-uint32_t vertexSSP(uint32_t v){
+struct qNode{
+	uint32_t node;
+	uint32_t distance;
+};
+
+// sum of the shortest paths from one vertex
+double vertexSSP(uint32_t v){
 	uint32_t sum = 0;
 	vector<bool> visited(numNodes, false);
-	vector<uint32_t> fringe(numNodes);
-	vector<uint32_t> newFringe(numNodes);
-	fringe[0] = v;
-	uint32_t d = 1;	
-	while(! fringe.empty()){
-		for(uint32_t x : fringe){
-			for(auto n : neighbours[x]){
-				if(! visited[n]){
-					newFringe.push_back(n);
-					visited[n] = true;
-					sum+=d;
-				}
+	queue<qNode> q;	// greater memory flexibility
+	q.push({v, 0}); visited[v] = true;
+	while(!q.empty()){
+		qNode current = q.front(); q.pop();
+		for(uint32_t n : neighbours[current.node]){
+			if(! visited[n]){
+				q.push({n, current.distance + 1});
+				sum += current.distance + 1;
+				visited[n] = true;
 			}
 		}
-		fringe.clear();
-		fringe.swap(newFringe);
-		d++;
-	}	
-	return sum;
-}
-
-// sum of shortest paths only to the vertices higher than this one
-double vertexHigherSSP(uint32_t v){
-	uint32_t sum = 0,d = 1;
-	vector<bool> visited(numNodes, false);
-	vector<uint32_t> fringe(numNodes - v);
-	vector<uint32_t> newFringe(numNodes - v);
-	fringe[0] = v;
-	while(! fringe.empty()){
-		for(uint32_t x : fringe){
-			for(auto n : neighbours[x]){
-				if(n > v && !visited[n-v-1]){
-					newFringe.push_back(n);
-					visited[n-v-1] = true;
-					sum+=d;
-				}
-			}
-		}
-		fringe.clear();
-		fringe.swap(newFringe);
-		d++;
 	}	
 	return sum;
 }
 
 double serialASP(){
-	double avg = 0.;
+	uint64_t avg = 0;
 	for(uint32_t v = 0; v < numNodes; v++){
-		avg += vertexSSP(v)* 1. / (numNodes - 1);
+		avg += vertexSSP(v);
 	}
-	return avg /(1.0 * numNodes);
+	return avg * 1.0 /(numNodes * (numNodes - 1));
 
 }
-
-double serialHigherASP(){
-	uint64_t avg = 0.;
-	for(uint32_t v = 0; v < numNodes; v++){
-		avg += vertexHigherSSP(v);
-	}
-	return avg * 2.0 /(numNodes * (numNodes - 1));
-
-}
-
 
 int main(){
 	string path = "/home/hubert/Code/Warwick/BSP/external/asp/Networks/n5.edges";
 	readGraph(path);
-	for(uint32_t v = 0; v < numNodes; v++){
-		// cout<<vertexSSP(v)<<endl;
-	}
-	// cout<< serialASP()<<endl;
+	cout<< serialASP()<<endl;
 	return 0;
 }
 
