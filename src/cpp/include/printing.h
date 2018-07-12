b33a78f6b41076b366f198a35ae17a6d27b52616
Ddedalus <h.beres@hotmail.com>
2018-07-12 11:07:26 +0100

    Fix map printing overload

--- src/cpp/include/printing.h
+++ src/cpp/include/printing.h
@@ -15,8 +15,8 @@ template <class K, class V>
 std::ostream &operator<<(std::ostream &os, const std::unordered_map<K, V> &m)
 {
     os << "{";
-    for (const V &p : m)
-        os << p.first << ":" << p.second << ", ";
+    for (auto p = m.begin(); p != m.end(); ++p)
+        os << p->first << ":" << p->second << ", ";
     os << "}";
     return os;
 };
