62f20e44d6a8bf1de97d42b5018c64ebd2670b34
Ddedalus <h.beres@hotmail.com>
2018-07-13 11:42:01 +0100

    Rearranging files

--- src/TSP/tsplib_generators/Utilities.h
+++
@@ -1,4 +0,0 @@
-extern void fatal (char *s);
-extern void **createMatrix (int i, int j, int s);
-extern void sprand (int seed);
-extern int  lprand (void);
--- src/cpp/tests/tests.cpp
+++
@@ -1,9 +0,0 @@
-#include "tests.h"
-
-void tests(){
-    std::cout << "   ---   Testing   ---   \n" << std::endl;
-    // bitset_test();
-
-    graph_io_test();
-    std::cout << " --- Finished Testing --- \n" << std::endl;
-}
