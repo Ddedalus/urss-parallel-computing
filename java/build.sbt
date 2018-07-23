e5e96425dfdc4577e59b766b044fe69f26cd4c46
Ddedalus <h.beres@hotmail.com>
2018-07-23 14:09:34 +0100

    Working Kamon test configuration

--- java/build.sbt
+++ java/build.sbt
@@ -1,4 +1,4 @@
-name := "asp-scala"
+name := "akka-asp"
 
 version := "1.0"
 
@@ -6,19 +6,16 @@ scalaVersion := "2.12.6"
 
 lazy val akkaVersion = "2.5.8"
 
-compileOrder := CompileOrder.JavaThenScala
+cancelable in Global := true
 
+compileOrder := CompileOrder.JavaThenScala
 
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % akkaVersion,
-  "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
-  "org.scalatest" %% "scalatest" % "3.0.5" % "test"
 )
 libraryDependencies ++= Seq(
   "io.kamon" %% "kamon-core" % "1.0.1",
-  "io.kamon" %% "kamon-logback" % "1.0.0",
   "io.kamon" %% "kamon-akka-2.5" % "1.0.1",
-  "io.kamon" %% "kamon-prometheus" % "1.0.0",
-  "io.kamon" %% "kamon-zipkin" % "1.0.0")
+  "io.kamon" %% "kamon-prometheus" % "1.0.0")
 
 fork in run := true
--- java/src/main/scala/hubert/akka/AkkaASP.scala
+++ java/src/main/scala/hubert/akka/AkkaASP.scala
@@ -2,22 +2,72 @@ package hubert.akka
 
 import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
 
+// import org.slf4j.Logger
+// import org.slf4j.LoggerFactory
+
 import kamon.Kamon
 import kamon.prometheus.PrometheusReporter
-import kamon.zipkin.ZipkinReporter
+
+case object PingMessage
+case object PongMessage
+case object StartMessage
+case object StopMessage
+
+class Ping(pong: ActorRef) extends Actor {
+  var count = 0
+  def incrementAndPrint { count += 1}
+  def receive = {
+    case StartMessage =>
+        incrementAndPrint
+        pong ! PingMessage
+    case PongMessage => 
+        incrementAndPrint
+        if (count > 100000000) {
+          sender ! StopMessage
+          println("ping stopped")
+          context.stop(self)
+        } else {
+          sender ! PingMessage
+        }
+  }
+}
+
+class Pong extends Actor {
+  def receive = {
+    case PingMessage =>
+        // println("  pong")
+        sender ! PongMessage
+    case StopMessage =>
+        println("pong stopped")
+        context.stop(self)
+  }
+}
+
 
 object AkkaASP extends App {
   import hubert.akka.Master._
 
   Kamon.addReporter(new PrometheusReporter())
-  Kamon.addReporter(new ZipkinReporter())
+  // println(System.getenv());
+  // Kamon.addReporter(new ZipkinReporter())
 
-  val filename = "/home/hubert/Code/Warwick/BSP/data/newcastle/n5.edges"
-  val system: ActorSystem = ActorSystem("asp")
+  val system = ActorSystem("PingPongSystem")
+  val pong = system.actorOf(Props[Pong], name = "pong")
+  println(pong + " - actor pong")
+  val ping = system.actorOf(Props(new Ping(pong)), name = "ping")
+  // start them going
+  ping ! StartMessage
 
-  val master = system.actorOf(Master.props(filename), "master")
-  println(master)
-  master ! BuildGraph
-  master ! RequestASP(2)
-  System.in.read();
+  Thread.sleep(100000)
+  system.terminate
+
+  // val filename = "/home/hubert/Code/Warwick/BSP/data/newcastle/n5.edges"
+  // val system: ActorSystem = ActorSystem("asp")
+
+  // val master = system.actorOf(Master.props(filename), "master")
+  // println(master)
+  // master ! BuildGraph
+  // master ! RequestASP(2)
+  // System.in.read();
+  // Kamon.stop()
 }
