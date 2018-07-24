88269bb43971047c10ee2a78b112d627cdbffecd
Ddedalus <h.beres@hotmail.com>
2018-07-24 09:39:47 +0100

    Work on general search in progress.

--- java/src/main/scala/hubert/akka/Master.scala
+++ java/src/main/scala/hubert/akka/Master.scala
@@ -11,8 +11,6 @@ object Master {
 
   def props(filename: String): Props = Props(new Master(filename))
 
-  case object BuildGraph
-
   final case class RequestASP(val source: Int)
 
   final case class UpdateEstimate(distance: Double)
@@ -46,12 +44,13 @@ class Master(filename: String) extends Actor with ActorLogging {
   var idToNode: scala.collection.Map[Int, ActorRef] = _
 
   var finishedCount: Int = _
-  var sourceID: Int = _
+  var sourceID: Int = -1
 
   def onBuildGraph(): Unit = {
     log.warning("In BuildGraph")
     this.idToNode = this.graph.map {
-      case (key, v) => key -> context.actorOf(Node.props, "node-" + key)
+      case (key, v) =>
+        key -> context.actorOf(Node.props(sourceID), "node-" + key)
     }
     this.all_nodes = this.graph.map {
       case (id, ne) => idToNode(id) -> new NodeInfo(id)
@@ -87,13 +86,22 @@ class Master(filename: String) extends Actor with ActorLogging {
     if (this.finishedCount == all_nodes.size)
       log.warning(
         times_ful + "th ssp: " + all_nodes.foldLeft(0.0)(_ + _._2.distance))
-      times_ful += 1
+    times_ful += 1
+
   }
 
   def receive = {
-    case BuildGraph          => onBuildGraph()
-    case RequestASP(src)     => onRequestASP(src)
+    case RequestASP(src) => {
+      if (this.sourceID < 0) {
+        this.sourceID = src; onBuildGraph();
+      }
+      onRequestASP(src)
+    }
     case UpdateEstimate(est) => onUpdateEstimate(est)
     case GatherResults       => onGatherResults()
   }
+}
+
+class BulkMaster(filename: String) extends Master{
+  log
 }
--- java/src/main/scala/hubert/akka/Node.scala
+++ java/src/main/scala/hubert/akka/Node.scala
@@ -2,58 +2,70 @@ package hubert.akka
 
 import java.util.ArrayList
 
-import akka.actor.{ Actor, ActorLogging, ActorRef, ActorSystem, Props }
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
 
 //remove if not needed
 import scala.collection.JavaConversions._
 
 object Node {
 
-	def props(): Props = Props(new Node())
+  def props(src: Int): Props = Props(new Node(src))
 
-	final case class Neighbours(neighbours: Array[ActorRef])
+  final case class Neighbours(neighbours: Array[ActorRef])
 
-	final case class DistanceEstimate(distance: Double)
+  final case class DistanceEstimate(distance: Double)
 
-	case object AskDistance
+  final case class NewSource(sourceId: Int)
 
-	case object Propagate
+  case object AskDistance
+
+  case object Propagate
 }
 
-class Node extends Actor {
-	import hubert.akka.Master.{NotFinished, UpdateEstimate}
-	import Node._
+class Node(src: Int) extends Actor {
+  import hubert.akka.Master.{NotFinished, UpdateEstimate}
+  import Node._
 
-	private var neigh: Array[ActorRef] = _
-	private var dist: Double = Double.MaxValue
-	private var has_propagated: Boolean = true
-	private var supervisor: ActorRef = _
+  private var neigh: Array[ActorRef] = _
+  private var dist: Double = Double.MaxValue
+  private var has_propagated: Boolean = true
+  private var supervisor: ActorRef = _
+  private var source = src
 
-	def onDistanceEstimate(distance: Double): Unit = {
-		if (distance >= this.dist) return
-		this.dist = distance
-		if (!this.has_propagated) return
-		// send the messages just for the first time, clear mailbox then
-		context.parent ! NotFinished
-		self ! Node.Propagate
-		this.has_propagated = false
-	}
 
-	def onPropagate(): Unit = {
-		for (n <- this.neigh) {
-			n ! DistanceEstimate(this.dist + 1)
-		}
-		this.has_propagated = true
-		self ! AskDistance
-	}
+  def onDistanceEstimate(distance: Double): Unit = {
+    if (distance >= this.dist) return
+    this.dist = distance
+    if (!this.has_propagated) return
+    // send the messages just for the first time, clear mailbox then
+    context.parent ! NotFinished
+    self ! Node.Propagate
+    this.has_propagated = false
+    
+  }
 
-	def receive = {
-		case DistanceEstimate(dist) => onDistanceEstimate(dist);
-		case Propagate => onPropagate();
-		case AskDistance => {
-			if (has_propagated)
-				context.parent ! UpdateEstimate(this.dist)
-		}
-		case Neighbours(nb) => this.neigh = nb;		
-	}
+  def onPropagate(): Unit = {
+    for (n <- this.neigh) {
+      n ! DistanceEstimate(this.dist + 1)
+    }
+    this.has_propagated = true
+    self ! AskDistance
+  }
+
+  def onNewSource(src: Int): Unit = {
+    this.dist = Double.MaxValue
+    this.has_propagated = true
+    this.source = src
+  }
+
+  def receive = {
+    case DistanceEstimate(dist) => onDistanceEstimate(dist);
+    case Propagate              => onPropagate();
+    case AskDistance => {
+      if (has_propagated)
+        context.parent ! UpdateEstimate(this.dist)
+    }
+    case Neighbours(nb) => this.neigh = nb;
+    case NewSource(src) => onNewSource(src);
+  }
 }
