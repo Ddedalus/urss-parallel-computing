188d9eae653879486edb0c08d6f7971a8046bcc9
Ddedalus <h.beres@hotmail.com>
2018-07-25 13:28:59 +0100

    Code on which Prometheus metrics were taken

--- java/src/main/scala/hubert/akka/AkkaASP.scala
+++ java/src/main/scala/hubert/akka/AkkaASP.scala
@@ -2,18 +2,17 @@ package hubert.akka
 
 import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
 
-// import kamon.Kamon
-// import kamon.prometheus.PrometheusReporter
+import kamon.Kamon
+import kamon.prometheus.PrometheusReporter
 
 object AkkaASP extends App {
   import hubert.akka.Master._
   import hubert.akka.BulkMaster._
 
-  // Kamon.addReporter(new PrometheusReporter())
-  // println(System.getenv());
+  Kamon.addReporter(new PrometheusReporter())
   // Kamon.addReporter(new ZipkinReporter())
 
-  val filename = "/home/hubert/Code/Warwick/BSP/data/sample/sample.graph.txt"
+  val filename = "/home/hubert/Code/Warwick/BSP/data/newcastle/n5.edges"
   val system: ActorSystem = ActorSystem("asp")
 
   val master = system.actorOf(BulkMaster.props(filename), "master")
